2225c768e26fc0a4561629cd3d5bd41539fca2f7
amriteshx1 <amriteshx18@gmail.com>
2025-04-17 19:16:55 +0530

    Add Homepage with GET routes & controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,8 @@
+exports.getHomepage = (req, res) => {
+    res.render('index', {title: 'Home'});
+}
+
 exports.getSignUp = (req,res) => {
     res.render('signup', {title : 'Sign Up'});
+    res.redirect('/');
 }
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const usersController = require("../controllers/usersController");
 const usersRouter = Router();
 
+usersRouter.get('/', usersController.getHomepage);
 usersRouter.get("/signup", usersController.getSignUp);
 
 module.exports = usersRouter;
